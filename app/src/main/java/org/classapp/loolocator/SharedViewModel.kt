7e42b22ea7fd2822f068597e2aa68023621b3fff
Chatchawarin Chatchavalwong <chatchawarin.chatchavalwong@agoda.com>
2024-05-12 22:09:49 +0700

    can't filter

--- app/src/main/java/org/classapp/loolocator/SharedViewModel.kt
+++ app/src/main/java/org/classapp/loolocator/SharedViewModel.kt
@@ -10,35 +10,10 @@ import androidx.lifecycle.ViewModel
 class SharedViewModel : ViewModel() {
     val latValue = mutableDoubleStateOf(0.0)
     val lonValue = mutableDoubleStateOf(0.0)
-    private val _maxRangeValue = MutableLiveData<Int>().apply { value = 10 }
-    val maxRangeValue: LiveData<Int> = _maxRangeValue
-    private val _haveMale = MutableLiveData<Boolean>().apply { value = false }
-    val haveMale: LiveData<Boolean> = _haveMale
-    private val _haveFemale = MutableLiveData<Boolean>().apply { value = false }
-    val haveFemale: LiveData<Boolean> = _haveFemale
-    private val _haveBaby = MutableLiveData<Boolean>().apply { value = false }
-    val haveBaby: LiveData<Boolean> = _haveBaby
-    private val _havePrayer = MutableLiveData<Boolean>().apply { value = false }
-    val havePrayer: LiveData<Boolean> = _havePrayer
-    private val _haveDisabled = MutableLiveData<Boolean>().apply { value = false }
-    val haveDisabled: LiveData<Boolean> = _haveDisabled
-
-    fun setMaxRangeValue(value: Int) {
-        _maxRangeValue.value = value
-    }
-    fun setHaveMale(have: Boolean) {
-        _haveMale.value = have
-    }
-    fun setHaveFemale(have: Boolean) {
-        _haveFemale.value = have
-    }
-    fun setHaveBaby(have: Boolean) {
-        _haveBaby.value = have
-    }
-    fun setHavePrayer(have: Boolean) {
-        _havePrayer.value = have
-    }
-    fun setHaveDisabled(have: Boolean) {
-        _haveDisabled.value = have
-    }
+    val maxRangeValue = mutableIntStateOf(5)
+    val haveMale = mutableStateOf(false)
+    val haveFemale = mutableStateOf(false)
+    val haveDisabled = mutableStateOf(false)
+    val haveBaby = mutableStateOf(false)
+    val havePrayer = mutableStateOf(false)
 }
